43fbc6a01cca4f4e939bafec4feb23c3bcaf1371
Joseph Orbegoso Pea <joe@trusktr.io>
2015-06-13 15:34:11 -0700

    Added .git to git URL.

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
     // Brief, one-line summary of the package.
     summary: 'ES6 Modules for Meteor. (And CJS/AMD too!)',
     // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/trusktr/rocket-module',
+    git: 'https://github.com/trusktr/rocket-module.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
