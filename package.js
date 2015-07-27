3b0b264ca862f06d1d31c53e2fce71396ae4f62d
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-27 15:50:56 -0700

    Update version.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'rocket:module',
-    version: '0.3.0',
+    version: '0.3.1',
     // Brief, one-line summary of the package.
     summary: 'ES6 Modules for Meteor. (And CJS/AMD too!)',
     // URL to the Git repository containing the source code for this package.
